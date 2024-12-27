3cf244b707592b49e6f2aed7941bf27ea86cd077
Mantragajjar <mantragajjar303@gmail.com>
2024-12-27 22:20:56 +0530

    3rd_commit

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array
+{
+    public static void main(String args[])
+    {
+        int arr[] = new int[4];
+        arr[0] = 2;
+        arr[1] = 5;
+        arr[2] = 6;
+        arr[3] = 8;
+
+        for (int i=0;i<4;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ BooleanPractice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class BooleanPractice
+{
+    public static void main(String ar[])
+    {
+        System.out.println("Enter your number");
+        Scanner sc=new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
